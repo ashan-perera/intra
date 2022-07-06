c606df6b536e885ba0d1357031c63d9036c47c9e
ashan-perera <bldashanperera14@gmail.com>
2022-07-06 16:00:00 +0530

    glry 2

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,3 +1,4 @@
+import { animate, AnimationEvent, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 interface Item {
@@ -8,18 +9,27 @@ interface Item {
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('150ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('150ms', style({transform: 'scale(0.5)'}))
+      ]),
+    ]),
+    trigger('animation2', [
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('500ms', style({opacity: 0.8}))
+      ])
+    ])
+  ]
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onPrevireImage(index: number): void {
-    
-  }
 
   galleryData = [
     {
@@ -75,5 +85,53 @@ export class GalleryComponent implements OnInit {
       imageAlt: '13'
     }
   ]
+
+  showCount = false;
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage: Item = this.galleryData[0];
+  currentIndex = 0;
+  controls = true;
+  totalImageCount = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.totalImageCount = this.galleryData.length;
+  }
+
+  onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightboxImage = this.galleryData[index];
+    this.showCount = true;
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    if(event.toState === 'void') {
+      this.showMask = false;
+    }
+  }
+
+  onClosePreview() {
+    this.previewImage = false;
+  }
+
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex > this.galleryData.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
+
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0) {
+      this.currentIndex = this.galleryData.length - 1;
+    }
+    this.currentLightboxImage = this.galleryData[this.currentIndex];
+  }
 
 }
