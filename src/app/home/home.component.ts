f2612135e028ab86542b4dae8327e87762a1248b
ashan-perera <bldashanperera14@gmail.com>
2022-07-26 15:29:20 +0530

    home change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,23 +21,26 @@ export class HomeComponent implements OnInit {
       'image': '../../assets/cicl.jpg'
     },
     { 
-      'id': 3,
-      'name': 'CICL Life',
-      'description': 'Navigate to CICL Life System',
-      'url': 'http://172.20.10.10:8080/CoopLife/',
-      'image': '../../assets/ciclLife.jpg'
-    },
-    { 
       'id': 2,
       'name': 'IRCSL',
       'description': 'Navigate to IRCSL',
       'url': 'http://172.20.10.10:8080/CoopLife/',
       'image': '../../assets/ircsl.jpg'
+    }
+  ]
+
+  usefulLinks2 = [
+    { 
+      'id': 1,
+      'name': 'CICL Life',
+      'description': 'Navigate to CICL Life System',
+      'url': 'http://172.20.10.10:8080/CoopLife/',
+      'image': '../../assets/ciclLife.jpg'
     },
     {
-      'id': 4,
+      'id': 2,
       'name': 'Learn Reactjs',
-      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      'description': 'Navigate to CICL Life System',
       'url': 'https://html.com/attributes/a-target/',
       'image': '../../assets/reactjs.jpg'
     }
